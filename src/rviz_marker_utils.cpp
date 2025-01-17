b4358b554dc822936ef360e72c3a45412fe2cdb2
Doncey Albin <donceyalbin@gmail.com>
2025-01-17 09:13:54 -0700

    Removed sphere marker method.

--- src/rviz_marker_utils.cpp
+++ src/rviz_marker_utils.cpp
@@ -5,76 +5,51 @@
 #include "lidar2osm_ros/rviz_marker_utils.hpp"
 #include "lidar2osm_ros/robot.hpp"
 
-visualization_msgs::msg::Marker getSphereMarker(rclcpp::Time current_time,
-                                                const double &normalized_distance,
-                                                const int currentMarkerIndex,
-                                                const double &distance,
-                                                const geometry_msgs::msg::Point &center_point)
-{
-    // Create a sphere marker
-    visualization_msgs::msg::Marker marker;
-    marker.header.frame_id = "world";
-    marker.header.stamp = current_time;
-    marker.ns = "sphere";
-    marker.id = currentMarkerIndex;
-    marker.type = visualization_msgs::msg::Marker::SPHERE;
-    marker.action = visualization_msgs::msg::Marker::ADD;
-    marker.pose.position.x = center_point.x;
-    marker.pose.position.y = center_point.y;
-    marker.pose.position.z = center_point.z;
-    marker.pose.orientation.x = 0.0;
-    marker.pose.orientation.y = 0.0;
-    marker.pose.orientation.z = 0.0;
-    marker.pose.orientation.w = 1.0;
-    marker.scale.x = distance; // Diameter of the sphere
-    marker.scale.y = distance;
-    marker.scale.z = distance;
-    marker.color.a = normalized_distance;
-    marker.color.r = normalized_distance;
-    marker.color.g = normalized_distance;
-    marker.color.b = normalized_distance;
-
-    return marker;
-}
-
-visualization_msgs::msg::Marker getLineMarker(const int robotNum,
+visualization_msgs::msg::Marker getLineMarker(const int robot1Num,
+                                              const int robot2Num,
+                                              const geometry_msgs::msg::TransformStamped& transform1,
+                                              const geometry_msgs::msg::TransformStamped& transform2,
                                               const int currentMarkerIndex,
-                                              rclcpp::Time current_time,
-                                              const double &normalized_distance, 
-                                              const geometry_msgs::msg::Point &center_point, 
-                                              const geometry_msgs::msg::TransformStamped &transform) 
+                                              rclcpp::Time current_time) 
 {
     // Init robot of interest
-    Robot robot;
-    robot.setRobot(robotNum);
+    Robot robot1;
+    Robot robot2;
+    robot1.setRobot(robot1Num + 1);
+    robot2.setRobot(robot2Num + 1);
 
     // Initialize the marker
     visualization_msgs::msg::Marker marker;
     marker.header.frame_id = "world";   // Set the reference frame
     marker.header.stamp = current_time;
-    marker.ns = "robot_distance_line" + currentMarkerIndex;
+    marker.ns = "robot_dist_line";
     marker.id = currentMarkerIndex;
     marker.type = visualization_msgs::msg::Marker::LINE_LIST;
     marker.action = visualization_msgs::msg::Marker::ADD;
 
     // Set the scale (line width)
-    marker.scale.x = 0.5*normalized_distance;
+    marker.scale.x = 1.0;
 
-    // Set the color (red with full alpha)
-    marker.color.r = robot.r;
-    marker.color.g = robot.g;
-    marker.color.b = robot.b;
+    // Set the color (ave of both robot's colors)
+    marker.color.r = (robot1.r + robot2.r) / 2;
+    marker.color.g = (robot1.g + robot2.g) / 2;
+    marker.color.b = (robot1.b + robot2.b) / 2;
     marker.color.a = 1.0;
 
     // Define the points (pairs of points form line segments)
-    geometry_msgs::msg::Point robot_position;
+    geometry_msgs::msg::Point robot1_position;
+    geometry_msgs::msg::Point robot2_position;
 
     // First line segment
-    robot_position.x = transform.transform.translation.x; 
-    robot_position.y = transform.transform.translation.y; 
-    robot_position.z = transform.transform.translation.z;
-    marker.points.push_back(center_point);
-    marker.points.push_back(robot_position);
+    robot1_position.x = transform1.transform.translation.x; 
+    robot1_position.y = transform1.transform.translation.y; 
+    robot1_position.z = transform1.transform.translation.z;
+    robot2_position.x = transform2.transform.translation.x; 
+    robot2_position.y = transform2.transform.translation.y; 
+    robot2_position.z = transform2.transform.translation.z;
+
+    marker.points.push_back(robot1_position);
+    marker.points.push_back(robot2_position);
 
     return marker;
 }
