a7370498b27b1d461d831bbcb43ebb52ef55d4c0
Doncey Albin <donceyalbin@gmail.com>
2025-01-17 09:11:07 -0700

    Added all robot colors.

--- include/lidar2osm_ros/robot.hpp
+++ include/lidar2osm_ros/robot.hpp
@@ -8,20 +8,25 @@ struct Robot {
 
     void setRobot(int robotNumber) {
         // Init robot of interest
-        if (robotNumber == 1) {
+        if (robotNumber == 1) {         // Green
+            r = 87.0/255.0; 
+            g = 227.0/255.0; 
+            b = 137.0/255.0;
+        }
+        else if (robotNumber == 2) {    // Purple
             r = 192.0/255.0; 
             g = 97.0/255.0; 
             b = 203.0/255.0;
         }
-        else if (robotNumber == 2) {
-            r = 87.0/255.0; 
-            g = 227.0/255.0; 
-            b = 137.0/255.0;
+        else if (robotNumber == 3) {    // Orange
+            r = 255.0/255.0; 
+            g = 163.0/255.0; 
+            b = 72.0/255.0;
         }
-        else if (robotNumber == 3) {
-            r = 87.0/255.0; 
-            g = 227.0/255.0; 
-            b = 137.0/255.0;
+        else if (robotNumber == 4) {    // Blue
+            r = 98.0/255.0; 
+            g = 160.0/255.0; 
+            b = 234.0/255.0;
         }
     }
 };
